9999e30cdeed908514d0c91d7f34ab040c184526
RomanYurkiv <yurkiv.r.official@gmail.com>
2023-12-15 16:42:28 +0200

    Done

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,22 @@
-function slugify(title) {
-    
-    const lowerCaseTitle = title.toLowerCase();
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce((acc, val) => acc + val, 0);
 
-    const words = lowerCaseTitle.split(" ");
-
-    const slug = words.join("-");
-
-    return slug;
+  return totalProducts <= containerSize;
 }
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
+
